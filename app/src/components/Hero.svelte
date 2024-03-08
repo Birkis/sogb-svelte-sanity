7f7089bced74265ec465cca37d15ef98f843a730
Michael Birkeland <60202353+Birkis@users.noreply.github.com>
2024-03-08 09:27:50 +0100

    Update Hero.svelte
    fjernet unødvendig link

--- app/src/components/Hero.svelte
+++ app/src/components/Hero.svelte
@@ -38,9 +38,9 @@
 							class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
 							>{content.hero.primaryCta.label}</a
 						>
-						<a href="{content.hero.secondaryCta.link}" class="text-sm font-semibold leading-6 text-gray-100"
+						<!-- <a href="{content.hero.secondaryCta.link}" class="text-sm font-semibold leading-6 text-gray-100"
 							>{content.hero.secondaryCta.label} <span aria-hidden="true">→</span></a
-						>
+						> -->
 					</div>
 				</div>
 			</div>
