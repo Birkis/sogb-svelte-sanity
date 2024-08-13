9260e6cacfc7c7e4f94217738e89b870541c56db
Michael Birkeland <60202353+Birkis@users.noreply.github.com>
2024-08-13 10:09:24 +0200

    Update Hero.svelte
    la til ny knapp på forsiden

--- app/src/components/Hero.svelte
+++ app/src/components/Hero.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { urlFor } from '$lib/utils/image';
-	import type { Hero } from '$lib/utils/sanity';
 	export let content: any;
 
 </script>
@@ -14,9 +13,9 @@
 				aria-hidden="true"
 			/>
 			<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-				<div class=" max-w-xs mx-auto my-2 sm:mb-8 sm:flex sm:justify-center">
+				<div class=" max-w-s mx-auto my-2 sm:mb-8 sm:flex sm:justify-center gap-2">
 					<div
-						class=" text-center md:text-left relative rounded-full px-3 py-1 text-sm leading-6 text-gray-100 ring-1 ring-gray-100/70 hover:ring-gray-900/20"
+						class=" text-center md:text-left relative rounded-full px-3 my-2 py-1 text-sm leading-6 text-gray-100 ring-1 ring-gray-100/70 hover:ring-gray-900/20"
 					>
 						Finn sikkerhetsdatablad. 
 							<a href="https://app.ecoonline.com/public/search-configuration/search?companyID=1002502&prodType=er&descrLang=1" class="font-bold text-white"
@@ -24,7 +23,17 @@
 							<span aria-hidden="true">&rarr;</span></a
 						>
 					</div>
+					<div
+					class=" text-center md:text-left relative rounded-full px-3 my-2 py-1 text-sm leading-6 text-gray-100 ring-1 ring-gray-100/70 hover:ring-gray-900/20"
+					>
+					Last ned returskjema. 
+						<a href="https://sorensenogbalchen.sharepoint.com/:b:/s/Hovedkontor/EcjcEni6O0dNsdLh615LvWMBazjxpSKs3D05tl_Lr9heAA?e=LutkhG" class="font-bold text-white"
+						><span class="absolute inset-0" aria-hidden="true" />Les mer
+						<span aria-hidden="true">&rarr;</span></a
+					>
 				</div>
+				</div>
+				
 				<div class="text-center">
 					<h1 class="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl">
 						{content.hero.heading}
